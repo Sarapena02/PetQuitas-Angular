9d50a972d982a4ca2fd4dcdf1c3fca5a88a8c85f
clarck2547 <juandavidh76@gmail.com>
2023-09-24 17:32:31 -0500

    detalle-mascotas

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TablaMascotasComponent } from './mascotas/tabla-mascotas/tabla-mascotas.component';
+import { MascotaDetalleComponent } from './mascotas/mascota-detalle/mascota-detalle.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TablaMascotasComponent
+    TablaMascotasComponent,
+    MascotaDetalleComponent
   ],
   imports: [
     BrowserModule,
