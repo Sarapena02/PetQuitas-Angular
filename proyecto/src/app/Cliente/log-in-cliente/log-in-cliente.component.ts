98a7988988952dd58f6bc62c834044f7f08204e5
Saya <fl.daniela@javeriana.edu.co>
2023-10-09 17:39:33 -0500

    Log in 2
    a

--- proyecto/src/app/Cliente/log-in-cliente/log-in-cliente.component.ts
+++ proyecto/src/app/Cliente/log-in-cliente/log-in-cliente.component.ts
@@ -6,30 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./log-in-cliente.component.css']
 })
 export class LogInClienteComponent {
-  cedula: string;
-  constructor() {
-    this.cedula = '';
-  }
-
   validarCedula() {
     // Obtiene el valor de la cédula ingresada por el usuario
-    const cedulaIngresada = document.getElementById("cedula") as HTMLInputElement;
-
-    if (cedulaIngresada) {
-      const cedula = cedulaIngresada.value;
-  
-      // Simulamos una respuesta del servidor (cedulaExiste es un booleano)
-      const cedulaExiste = false; // Cambia esto según la lógica real
-  
-      if (cedulaExiste) {
-        alert('La cédula ya existe.');
-      } else {
-        this.cedula = cedula;
-  
-        // Puedes agregar aquí cualquier otra lógica que necesites antes de enviar el formulario.
-      }
-    } else {
-      alert("Elemento 'cedula' no encontrado");
-    }
   }
 }
