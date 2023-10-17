24339f59558c2b13ae97c0cfa4be067050b426df
Saya <fl.daniela@javeriana.edu.co>
2023-10-16 19:30:21 -0500

    Update app.module.ts

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -14,6 +14,40 @@ import { FormMascotaComponent } from './mascotas/form-mascota/form-mascota.compo
 import { FormsModule, NgForm } from '@angular/forms';
 import { FormEditMascotaComponent } from './mascotas/form-edit-mascota/form-edit-mascota.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormEditClienteComponent } from './Cliente/form-edit-cliente/form-edit-cliente.component';
+import { ClienteDetalleComponent } from './Cliente/cliente-detalle/cliente-detalle.component';
+import { ClienteTablaComponent } from './Cliente/tabla-cliente/tabla-cliente.component';
+import { ClienteFormComponent } from './Cliente/form-cliente/cliente-form.component';
+import { LogInVeterinarioComponent } from './Veterinario/log-in-veterinario/log-in-veterinario.component';
+import { LogInClienteComponent } from './Cliente/log-in-cliente/log-in-cliente.component';
+import { LogInAdminComponent } from './Admin/log-in-admin/log-in-admin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardAdminComponent } from './Admin/dashboard-admin/dashboard-admin.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormEditVeterinarioComponent } from './Veterinario/form-edit-veterinario/form-edit-veterinario.component';
+import { FormVeterinarioComponent } from './Veterinario/form-veterinario/form-veterinario.component';
+import { VeterinarioDetalleComponent } from './Veterinario/veterinario-detalle/veterinario-detalle.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { TramientosUltimoMesComponent } from './Admin/InfoDashboard/tramientos-ultimo-mes/tramientos-ultimo-mes.component';
+import { CantidadTratamientosTipoComponent } from './Admin/InfoDashboard/cantidad-tratamientos-tipo/cantidad-tratamientos-tipo.component';
+import { VeterinariosActivosComponent } from './Admin/InfoDashboard/veterinarios-activos/veterinarios-activos.component';
+import { MascotasActivasComponent } from './Admin/InfoDashboard/mascotas-activas/mascotas-activas.component';
+import { VentasTotalesComponent } from './Admin/InfoDashboard/ventas-totales/ventas-totales.component';
+import { GananciasTotalesComponent } from './Admin/InfoDashboard/ganancias-totales/ganancias-totales.component';
+import { Top3TratamientosComponent } from './Admin/InfoDashboard/top3-tratamientos/top3-tratamientos.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import {MatTableModule} from '@angular/material/table';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormEditTratamientoComponent } from './Tratamiento/form-edit-tratamiento/form-edit-tratamiento.component';
+import { FormTratamientoComponent } from './Tratamiento/form-tratamiento/form-tratamiento.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -27,7 +61,26 @@ import { HttpClientModule } from '@angular/common/http';
     LandingComponent,
     ErrorPageComponent,
     FormMascotaComponent,
-    FormEditMascotaComponent
+    FormEditMascotaComponent,
+    FormEditClienteComponent,
+    ClienteDetalleComponent,
+    ClienteTablaComponent,
+    LogInVeterinarioComponent,
+    LogInClienteComponent,
+    LogInAdminComponent,
+    DashboardAdminComponent,
+    FormEditVeterinarioComponent,
+    FormVeterinarioComponent,
+    VeterinarioDetalleComponent,
+    TramientosUltimoMesComponent,
+    CantidadTratamientosTipoComponent,
+    VeterinariosActivosComponent,
+    MascotasActivasComponent,
+    VentasTotalesComponent,
+    GananciasTotalesComponent,
+    Top3TratamientosComponent,
+    FormEditTratamientoComponent,
+    FormTratamientoComponent,
   ],
   imports: [
     BrowserModule,
