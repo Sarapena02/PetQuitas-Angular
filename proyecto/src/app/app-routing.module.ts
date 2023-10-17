417d020433d89469bd87ade9f147fd87a457c351
Saya <fl.daniela@javeriana.edu.co>
2023-10-16 19:27:43 -0500

    Update app-routing.module.ts

--- proyecto/src/app/app-routing.module.ts
+++ proyecto/src/app/app-routing.module.ts
@@ -6,6 +6,19 @@ import { ErrorPageComponent } from './error/error-page/error-page.component';
 import { TablaMascotasComponent } from './mascotas/tabla-mascotas/tabla-mascotas.component';
 import { FormMascotaComponent } from './mascotas/form-mascota/form-mascota.component';
 import { FormEditMascotaComponent } from './mascotas/form-edit-mascota/form-edit-mascota.component';
+import { LogInClienteComponent } from './Cliente/log-in-cliente/log-in-cliente.component';
+import { LogInVeterinarioComponent } from './Veterinario/log-in-veterinario/log-in-veterinario.component';
+import { FormEditClienteComponent } from './Cliente/form-edit-cliente/form-edit-cliente.component';
+import { ClienteTablaComponent } from './Cliente/tabla-cliente/tabla-cliente.component';
+import { ClienteDetalleComponent } from './Cliente/cliente-detalle/cliente-detalle.component';
+import { ClienteFormComponent } from './Cliente/form-cliente/cliente-form.component';
+import { LogInAdminComponent } from './Admin/log-in-admin/log-in-admin.component';
+import { DashboardAdminComponent } from './Admin/dashboard-admin/dashboard-admin.component';
+import { VeterinarioTableComponent } from './Veterinario/veterinario-table/veterinario-table.component';
+import { VeterinarioDetalleComponent } from './Veterinario/veterinario-detalle/veterinario-detalle.component';
+import { FormEditVeterinarioComponent } from './Veterinario/form-edit-veterinario/form-edit-veterinario.component';
+import { FormVeterinarioComponent } from './Veterinario/form-veterinario/form-veterinario.component';
+import { FormTratamientoComponent } from './Tratamiento/form-tratamiento/form-tratamiento.component';
 
 const routes: Routes = [
   { path: 'mascota/find/:id', component: MascotaDetalleComponent },
@@ -17,8 +30,18 @@ const routes: Routes = [
   { path: 'clientes/all', component: ClienteTablaComponent },
   { path: 'cliente/find/:id', component: ClienteDetalleComponent },
   { path: 'home', component: LandingComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
-  { path: '**', component: ErrorPageComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },  
+  { path: 'logInCliente', component: LogInClienteComponent },
+  { path: 'logInVet', component: LogInVeterinarioComponent },
+  { path: 'logInAdmin', component: LogInAdminComponent },
+  { path: 'admin/dashboard', component: DashboardAdminComponent },
+  { path: 'veterinarios/all', component: VeterinarioTableComponent },
+  { path: 'veterinario/add', component: FormVeterinarioComponent },
+  { path: 'veterinario/edit/:id', component: FormEditVeterinarioComponent },
+  { path: 'veterinario/find/:id', component: VeterinarioDetalleComponent },
+  {path:  'tratamiento/add', component: FormTratamientoComponent},
+  { path: '**', component: ErrorPageComponent }
+
 ];
 
 @NgModule({
